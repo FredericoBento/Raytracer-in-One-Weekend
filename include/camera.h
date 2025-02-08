57718da8659ada5044fc8ca0407774b61337d925
Frederico Bento <fredbento39@gmail.com>
2025-02-08 17:27:58 +0000

    Chapter 9

--- include/camera.h
+++ include/camera.h
@@ -2,13 +2,15 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "vec3.h"
 
 class camera
 {
     public:
-        double aspect_ratio = 1.0;
-        int    image_width = 100;
+        double aspect_ratio      = 1.0;
+        int    image_width       = 100;
         int    samples_per_pixel = 10; // Count of random samples for each pixel
+        int    max_depth         = 10; // Maximum number of ray bounces into scene 
 
         void render(const hittable& world)
         {
@@ -26,7 +28,7 @@ class camera
                       for (int sample = 0; sample < samples_per_pixel; sample++)
                       {
                           ray r = get_ray(i, j);
-                          pixel_color += ray_color(r, world);
+                          pixel_color += ray_color(r, max_depth, world);
                       }
                       write_color(std::cout, pixel_samples_scale * pixel_color);
                   }
@@ -95,12 +97,17 @@ class camera
             return vec3(random_double() - 0.5, random_double() - 0.5, 0);
         }
 
-        color ray_color(const ray& r, const hittable& world) const
+        color ray_color(const ray& r, int depth, const hittable& world) const
         {
+            if (depth <= 0)
+                return color(0, 0, 0);
+
             hit_record rec;
 
-            if (world.hit(r, interval(0, infinity), rec)) {
-                return 0.5 * (rec.normal + color(1,1,1));
+            if (world.hit(r, interval(0.001, infinity), rec)) {
+                // vec3 direction = random_on_hemisphere(rec.normal);
+                vec3 direction = rec.normal + random_unit_vector();
+                return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
             }
 
             vec3 unit_direction = unit_vector(r.direction());
